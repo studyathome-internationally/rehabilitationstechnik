88e6ba69a1b2540b4edc1027c4e275933f3572a9
Alija Sabic <sabic.alija@gmail.com>
2022-04-11 02:54:53 +0200

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yarn docs:build
+yarn docs:build --clean-cache --clean-temp
 scp -r docs/.vuepress/dist/* study@studyathome.technikum-wien.at:/var/www/html/asterics-web-devwindows/
 ssh study@studyathome.technikum-wien.at chmod -R 755 /var/www/html/asterics-web-devwindows/
