d46441231cb2066975e98c6f0c581407b31b79a7
Alija Sabic <sabic.alija@gmail.com>
2022-06-22 20:44:56 +0200

    Prepare MathML styling and component for release

--- docs/.vuepress/components/MathML.vue
+++ docs/.vuepress/components/MathML.vue
@@ -5,22 +5,44 @@
 
 <script setup lang="ts">
 import { computed, toRefs, onMounted } from "vue";
+import katex from "katex";
 
-import { mathjax } from "mathjax-full/js/mathjax.js";
-import { TeX } from "mathjax-full/js/input/tex.js";
-import { SVG } from "mathjax-full/js/output/svg.js";
-import { liteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor.js";
-import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html.js";
-import { AssistiveMmlHandler } from "mathjax-full/js/a11y/assistive-mml.js";
-import { AllPackages } from "mathjax-full/js/input/tex/AllPackages.js";
-import juice from "juice/client";
+// import { mathjax } from "mathjax-full/js/mathjax.js";
+// import { TeX } from "mathjax-full/js/input/tex.js";
+// import { SVG } from "mathjax-full/js/output/svg.js";
+// import { liteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor.js";
+// import { RegisterHTMLHandler } from "mathjax-full/js/handlers/html.js";
+// import { AssistiveMmlHandler } from "mathjax-full/js/a11y/assistive-mml.js";
+// import { AllPackages } from "mathjax-full/js/input/tex/AllPackages.js";
+// import juice from "juice/client";
 
-const documentOptions = {
-  InputJax: new TeX({ packages: AllPackages }),
-  OutputJax: new SVG({ fontCache: "none" }),
-};
-const convertOptions = {
-  display: false,
+// const documentOptions = {
+//   InputJax: new TeX({ packages: AllPackages }),
+//   OutputJax: new SVG({ fontCache: "none" }),
+// };
+// const convertOptions = {
+//   display: false,
+// };
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+const katexInline = function (latex) {
+  const options = { displayMode: false, strict: "ignore", throwOnError: false };
+  try {
+    return katex.renderToString(latex, options);
+  } catch (error) {
+    if (options.throwOnError) {
+      console.log(error);
+    }
+    return `<span v-pre class='katex-error' title='${escapeHtml(error.toString())}'>${escapeHtml(latex)}</span>`;
+  }
 };
 
 const props = defineProps({
@@ -38,13 +60,14 @@ const props = defineProps({
 const { formula } = toRefs(props);
 
 const mlContent = computed(() => {
-  const adaptor = liteAdaptor();
-  const handler = RegisterHTMLHandler(adaptor);
-  AssistiveMmlHandler(handler);
-  const mathDocument = mathjax.document(formula.value, documentOptions);
-  const html = adaptor.outerHTML(mathDocument.convert(formula.value, convertOptions));
-  const stylesheet = adaptor.outerHTML(documentOptions.OutputJax.styleSheet(mathDocument));
-  return juice(html + stylesheet);
+  //   const adaptor = liteAdaptor();
+  //   const handler = RegisterHTMLHandler(adaptor);
+  //   AssistiveMmlHandler(handler);
+  //   const mathDocument = mathjax.document(formula.value, documentOptions);
+  //   const html = adaptor.outerHTML(mathDocument.convert(formula.value, convertOptions));
+  //   const stylesheet = adaptor.outerHTML(documentOptions.OutputJax.styleSheet(mathDocument));
+  //   return juice(html + stylesheet);
+  return katexInline(formula.value).replace(`<span class="katex">`, `<span v-pre class="katex">`);
 });
 </script>
 
