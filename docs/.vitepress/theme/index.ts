6a17cf6926cfa56580d909ca2f910ae428ccbd93
Alija Sabic <sabic.alija@gmail.com>
2023-02-17 13:09:59 +0100

    Setup medium zoom on mount and path updates

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,11 @@ import BookUrl from "./components/BookUrl.vue";
 import ChapterUrl from "./components/ChapterUrl.vue";
 import MathML from "./components/MathML.vue";
 
+import mediumZoom from "medium-zoom";
+
+import { onMounted, watch, nextTick } from "vue";
+import { inBrowser, useRoute } from "vitepress";
+
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
@@ -12,5 +17,14 @@ export default {
     ctx.app.component("BookUrl", BookUrl);
     ctx.app.component("ChapterUrl", ChapterUrl);
     ctx.app.component("MathML", MathML);
+  },
+  setup() {
+    const initZoom = () => {
+      if (inBrowser) mediumZoom(".vp-doc img", { background: "var(--vp-c-bg)", margin: 100 });
+    };
+    const route = useRoute();
+    const handler = () => nextTick(initZoom);
+    onMounted(initZoom);
+    watch(() => route.path, handler);
   },
 };
