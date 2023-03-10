095969bf8b91d6db2635b41555f3707866c5bd76
Alija Sabic <sabic.alija@gmail.com>
2023-03-10 10:08:48 +0100

    Configure katex to output mathml only, add katex macros as workaround for missing siunitx package support

--- docs/.vitepress/config/plugins/math.ts
+++ docs/.vitepress/config/plugins/math.ts
@@ -195,6 +195,40 @@ function math_plugin(md, options) {
   // Default options
 
   options = options || {};
+  options.output = "mathml";
+  options.macros = {
+    "\\SI": "{#1\\;\\mathrm{#2}}",
+    "\\squared": "{^{2}}",
+    "\\cubed": "{^{3}}",
+    "\\per": "/",
+    "\\tera": "T",
+    "\\giga": "G",
+    "\\mega": "M",
+    "\\kilo": "k",
+    "\\milli": "m",
+    "\\micro": "μ",
+    "\\nano": "n",
+    "\\kilogram": "\\text{kg}\\,",
+    "\\meter": "\\text{m}\\,",
+    "\\second": "\\text{s}\\,",
+    "\\ampere": "\\text{A}\\,",
+    "\\kelvin": "\\text{K}\\,",
+    "\\mol": "\\text{mol}\\,",
+    "\\candela": "\\text{cd}\\,",
+    "\\newton": "\\text{N}\\,",
+    "\\hertz": "\\text{Hz}\\,",
+    "\\pascal": "\\text{Pa}\\,",
+    "\\volt": "\\text{V}\\,",
+    "\\watt": "\\text{W}\\,",
+    "\\joule": "\\text{J}\\,",
+    "\\henry": "\\text{H}\\,",
+    "\\farad": "\\text{F}\\,",
+    "\\coulomb": "\\text{C}\\,",
+    "\\ohm": "\\Omega\\,",
+    "\\weber": "\\text{Wb}\\,",
+    "\\tesla": "\\text{T}\\,",
+    "\\degree": "\\text{deg}\\,",
+  };
 
   // set KaTeX as the renderer for markdown-it-simplemath
   var katexInline = function (latex) {
